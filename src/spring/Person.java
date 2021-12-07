9b3b63baa9fb9e37d2dcd90c90fd4fc9c2d836f8
Dmitry Yakovenko <jads84@gmail.com>
2021-12-07 21:42:20 +0300

    1.8 DI xml with setter

--- src/spring/Person.java
+++ src/spring/Person.java
@@ -2,8 +2,18 @@ public class Person {
 
     private Pet pet;
 
+    public Person() {
+        System.out.println("Person bean is created");
+    }
+
     public Person(Pet pet) {
         System.out.println("Person bean is created");
+        this.pet = pet;
+    }
+
+    // pet -> setPet
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set pet");
         this.pet = pet;
     }
 
