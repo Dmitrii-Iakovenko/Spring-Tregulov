705f0f6466c05ccea3d2fc53f33e27416c492458
Dmitry Yakovenko <jads84@gmail.com>
2021-12-07 21:49:53 +0300

    1.9 DI xml: injection value

--- src/spring/Person.java
+++ src/spring/Person.java
@@ -1,6 +1,8 @@
 public class Person {
 
     private Pet pet;
+    private String surname;
+    private int age;
 
     public Person() {
         System.out.println("Person bean is created");
@@ -15,6 +17,24 @@ public class Person {
     public void setPet(Pet pet) {
         System.out.println("Class Person: set pet");
         this.pet = pet;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        System.out.println("Class Person: set surname");
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        System.out.println("Class Person: set age");
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public void callYourPet() {
