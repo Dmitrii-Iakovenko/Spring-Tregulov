8c6b598ce3e0adee0755db6128b5f0320f9692d3
Dmitriy Yakovenko <jads84@gmail.com>
2021-12-14 19:28:54 +0300

    1.23 @PropertySource

--- src/java/spring/Test6.java
+++ src/java/spring/Test6.java
@@ -8,7 +8,9 @@ public class Test6 {
                 new AnnotationConfigApplicationContext(MyConfig.class);
 
         Person person = context.getBean("personBean", Person.class);
-        person.callYourPet();
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
+//        person.callYourPet();
 
 //        Pet cat1 = context.getBean("catBean", Pet.class);
 //        Pet cat2 = context.getBean("catBean", Pet.class);
