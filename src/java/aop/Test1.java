14239a806a8bc01114d9e4a3c5844f82ea828444
Dmitriy Yakovenko <jads84@gmail.com>
2021-12-15 17:33:20 +0300

    2.27 Pointcut part2

--- src/java/aop/Test1.java
+++ src/java/aop/Test1.java
@@ -9,8 +9,12 @@ public class Test1 {
                 new AnnotationConfigApplicationContext(MyConfig.class);
 
         UniLibrary uniLibrary = context.getBean("libraryBean", UniLibrary.class);
-//        uniLibrary.getBook();
-        uniLibrary.returnBook();
+        Book book = context.getBean("book", Book.class);
+
+        uniLibrary.getBook(book);
+
+
+        //        uniLibrary.returnBook();
 //        uniLibrary.getMagazine();
 
 //        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
