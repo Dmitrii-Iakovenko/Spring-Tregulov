c6053dbaaea3e4e396ad631a6f61c212d69276f3
Dmitry Yakovenko <jads84@gmail.com>
2021-12-26 15:28:53 +0300

    3.47 @OneToOne Uni-directional 1

--- src/resources/2.sql
+++ src/resources/2.sql
@@ -1,16 +1,19 @@
--- CREATE DATABASE  my_db;
--- USE my_db;
---
---
+-- DROP TABLE employees;
 
-CREATE TABLE employees (
---   id int NOT NULL AUTO_INCREMENT,
---   id BIGSERIAL PRIMARY KEY NOT NULL,
-  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name varchar(15),
-  surname varchar(25),
-  department varchar(20),
-  salary int
---   PRIMARY KEY (id)
+
+CREATE TABLE details (
+     id  BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+     city varchar(15),
+     phone_number varchar(25),
+     email varchar(30)
 );
 
+CREATE TABLE employees (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name varchar(15),
+    surname varchar(25),
+    department varchar(20),
+    salary int,
+    details_id BIGINT,
+    FOREIGN KEY (details_id) REFERENCES details(id)
+);
